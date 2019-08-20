27e7dc9610bf8282d4ddc4d07c7db0a55e41deb5
Justin Grimes <zelon88@gmail.com>
2019-08-20 10:10:32 -0400

    Add more useful JSFunctions.

--- Scripts/Javascript/JSFunctions.js
+++ Scripts/Javascript/JSFunctions.js
@@ -18,3 +18,43 @@ function humanDate() {
   var dateVar = new Date(); 
   return dateVar.toLocaleString(); }
 // / --------------------
+
+// / --------------------
+// / A function to call the VB sub saveSettings which just displays the "Save Complete" message.
+function callVBSave() {
+  saveSettings(); }
+// / --------------------
+
+
+// / --------------------
+// / A function to read the contents of a text file and replace the <body> of a page with it's contents.
+function readFile(path) { 
+  var fso = new ActiveXObject('Scripting.FileSystemObject'),
+    iStream=fso.OpenTextFile(path, 1, false);
+  while(!iStream.AtEndOfStream) { 
+    document.body.innerHTML += iStream.ReadLine() + '<br/>'; }        
+  iStream.Close(); }
+// / --------------------
+
+
+// / --------------------
+// / A function to read the contents of a text file and return the results.
+function readFile2(path) { 
+  var fso = new ActiveXObject('Scripting.FileSystemObject'),
+    iStream=fso.OpenTextFile(path, 1, false);
+  var data = "";
+  while(!iStream.AtEndOfStream) { 
+    data += iStream.ReadLine() + '<br/>'; }        
+  iStream.Close(); 
+  return data; }
+// / --------------------
+
+// / --------------------
+// / A function to toggle the visibility of the selected element between "block" and "none".
+function toggleVisibility(id) {
+  var e = document.getElementById(id);
+  if(e.style.display == 'block')
+     e.style.display = 'none';
+  else
+     e.style.display = 'block'; }
+// / --------------------
